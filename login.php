e026c351a31c77555da76eb19c46d5e728126e85
Niu3 <antonin.drapeau@outlook.fr>
2017-09-06 18:06:25 +0200

    site deposer sur git

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>3lastik l Joueur</title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="jquery-3.2.1.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+        <script src="JS/main.js"></script>
+        <link rel="stylesheet" type="text/css" href="CSS/styleLogin.css">
+        <link rel="icon" type="image/ico" href="favicon.ico">
+    </head>
+    <body>
+    <section id="">
+    	<div class="row">
+    		<div class="col-xs-12 col-lg-12">
+	    		<div class="education-block">
+		    			<span class="glyphicon glyphicon-log-in"></span>
+		    				<h1>Login | Register</h1></br>
+		    					<h4>I already have a Cs:Go Saphhire account</h4>
+		    				<div class="blue-divider"></div>
+		    					<h4>If you have CS:GO Saphhire account you can login here</h4>
+		    			   <form action="login-inc.php">
+                        <div class="usernameStyle">
+			    				<input class="inputUsername" type="text" name="Username" placeholder="Username (CS:GO Saphhire Account)">
+			    		</div>
+			    		<div class="passwordStyle">
+			    			<h2>Password</h2>
+			    				<input class="inputPassword" type="Password" name="Username">
+		    		</div>
+                    </form>
+	    		</div>
+	    	</div>
+    	</div>
+    </section>
+    	<a href=""><img class="imageLogo" src="IMGS/logo.png"></a>
+    </body>
+</html>
